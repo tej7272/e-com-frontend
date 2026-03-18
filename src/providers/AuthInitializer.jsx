22ad68a436fa0192a1bee60721804b74d371277b
tej7272 <tej7272@gmail.com>
2026-03-18 22:44:22 +0530

    done

--- src/providers/AuthInitializer.jsx
+++ src/providers/AuthInitializer.jsx
@@ -1,23 +1,21 @@
 // providers/AuthInitializer.jsx
-import { useEffect }            from 'react'
-import { useDispatch }          from 'react-redux'
-import { useLocation }          from 'react-router-dom'
-import { refreshAdminToken }    from 'store/slices/auth/adminAuthSlice'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useLocation } from 'react-router-dom'
+import { refreshAdminToken } from 'store/slices/auth/adminAuthSlice'
 import { refreshCustomerToken } from 'store/slices/auth/customerAuthSlice'
 
 const AuthInitializer = () => {
-  const dispatch     = useDispatch()
+  const dispatch = useDispatch()
   const { pathname } = useLocation()
 
   useEffect(() => {
     if (pathname.startsWith('/admin')) {
-      // ✅ Only fire admin refresh on admin routes
       dispatch(refreshAdminToken())
     } else {
-      // ✅ Only fire customer refresh on customer routes
       dispatch(refreshCustomerToken())
     }
-  }, []) // ← runs once on mount only
+  }, [])
 
   return null
 }
